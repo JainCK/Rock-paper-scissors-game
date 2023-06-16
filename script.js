e5b0284a9e81b8696c0357c8b13c9437338dee3a
zippyzag88 <kuriakosejain456@gmail.com>
2023-06-16 15:33:55 +0530

    js file for the required game

---
+++ script.js
@@ -0,0 +1,31 @@
+// Function to get the computer's choice
+function getComputerChoice() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+  }
+  
+  // Function to play a single round of Rock Paper Scissors
+  function playRound(playerSelection, computerSelection) {
+    // Convert playerSelection to lowercase for case-insensitivity
+    playerSelection = playerSelection.toLowerCase();
+  
+    // Determine the winner
+    if (playerSelection === computerSelection.toLowerCase()) {
+      return "It's a tie!";
+    } else if (
+      (playerSelection === "rock" && computerSelection === "Scissors") ||
+      (playerSelection === "paper" && computerSelection === "Rock") ||
+      (playerSelection === "scissors" && computerSelection === "Paper")
+    ) {
+      return "You win! " + playerSelection + " beats " + computerSelection;
+    } else {
+      return "You lose! " + computerSelection + " beats " + playerSelection;
+    }
+  }
+  
+  // Example usage
+  const playerSelection = prompt("Enter your choice: Rock, Paper, or Scissors");
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+  
