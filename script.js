b0dfdb35a8c9734823f893ce43a165e318655baa
zippyzag88 <kuriakosejain456@gmail.com>
2023-10-10 19:43:26 +0530

    Added restart and scoreboard

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const buttons = document.querySelectorAll("button");
 
 let playerScore = 0;
 let computerScore = 0;
+let roundCount = 0;
 
 // Function to get the computer's choice
 function getComputerChoice() {
@@ -36,15 +37,22 @@ function updateScore(playerSelection) {
   resultBox.textContent = roundResult;
   scoreBox.textContent = `Player: ${playerScore} - Computer: ${computerScore}`;
 
+  roundCount++;
+
+
   // Check for a winner
-  if (playerScore >= 5) {
-    resultBox.textContent = "Player wins the game!";
-    disableButtons();
-  } else if (computerScore >= 5) {
-    resultBox.textContent = "Computer wins the game!";
+  if (roundCount >= 5) {
+    if (playerScore > computerScore) {
+      resultBox.textContent = "Player wins the game!";
+    } else if (computerScore > playerScore) {
+      resultBox.textContent = "Computer wins the game!";
+    } else {
+      resultBox.textContent = "It's a tie! The game ends in a draw.";
+    }
     disableButtons();
   }
 }
+
 
 // Function to disable buttons after the game is over
 function disableButtons() {
@@ -64,3 +72,27 @@ function handleClick(event) {
 buttons.forEach((button) => {
   button.addEventListener("click", handleClick);
 });
+
+function restartGame() {
+  playerScore = 0;
+  computerScore = 0;
+  roundCount = 0;
+  resultBox.textContent = "";
+  scoreBox.textContent = "Player: 0 - Computer: 0";
+  enableButtons();
+}
+
+// Function to enable buttons for a new game
+function enableButtons() {
+  buttons.forEach((button) => {
+    button.addEventListener("click", handleClick);
+    button.disabled = false;
+  });
+}
+
+// Add click event listener to the Restart button
+const restartButton = document.getElementById("Restart");
+restartButton.addEventListener("click", restartGame);
+
+// Enable buttons to start a new game
+enableButtons();
